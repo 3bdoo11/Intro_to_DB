fa6a8bdcf62c23edc90422ac30940583f3d5b6e0
Abdallah <abdallahmohamedama12@gmail.com>
2025-08-17 20:32:16 +0300

    style: refine code formatting and improve exception handling in MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,23 +1,23 @@
+
+
 import mysql.connector
-from mysql.connector import Error   
 
 def create_database():
     connection = None
     try:
-        
+        # Connect to MySQL server
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="63125975"  
+            password="63125975"   
         )
 
         if connection.is_connected():
             cursor = connection.cursor()
-            # create database if not exists
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:  
+    except mysql.connector.Error as e:   
         print("Error while connecting to MySQL:", e)
 
     finally:
