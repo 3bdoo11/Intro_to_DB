1ef05147855e10a9d56cf49643256abbd47723d0
Abdallah <abdallahmohamedama12@gmail.com>
2025-08-17 20:29:21 +0300

    style: clean up code formatting and remove unnecessary comments in MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,27 +1,23 @@
-#!/usr/bin/python3
-"""
-Script to create a database 'alx_book_store' in MySQL server
-"""
-
 import mysql.connector
 from mysql.connector import Error   
 
 def create_database():
     connection = None
     try:
-        # Connect to MySQL server
+        
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="63125975"   
+            password="63125975"  
         )
 
         if connection.is_connected():
             cursor = connection.cursor()
+            # create database if not exists
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:   
+    except Error as e:  
         print("Error while connecting to MySQL:", e)
 
     finally:
